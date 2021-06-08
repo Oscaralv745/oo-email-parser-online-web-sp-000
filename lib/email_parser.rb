e670693267d9759e70e044aabd886e595dfe1972
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-08 01:59:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,9 +3,9 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor: emails 
+  attr_accessor: emails
   
   def parse
-    
+
   end
 end
